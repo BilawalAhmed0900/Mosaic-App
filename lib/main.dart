78888f755baab49f3b0d630a69e02a81939ed25f
Bilawal Ahmed <blwal7057@gmail.com>
2023-06-27 11:30:22 +0500

    Updated main for amplify

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_storage_s3/amplify_storage_s3.dart';
 import 'package:flutter/material.dart';
 import 'package:mosaic_app/Scaffolds/splash_screen_scaffold.dart';
 
-void main() {
+import 'amplifyconfiguration.dart';
+
+void main() async {
+  await Amplify.addPlugin(AmplifyStorageS3());
+  await Amplify.addPlugin(AmplifyAuthCognito());
+  await Amplify.configure(amplifyconfig);
+
   runApp(MaterialApp(
     theme: ThemeData(
       brightness: Brightness.light,
